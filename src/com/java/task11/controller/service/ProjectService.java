25ab28a1575a2ce0497a1077f103344a7215e445
demaW <demavov@gmail.com>
2014-03-14 14:04:21 +0200

    adding save and get list in project service

--- src/com/java/task11/controller/service/ProjectService.java
+++ src/com/java/task11/controller/service/ProjectService.java
@@ -1,38 +1,61 @@
 package com.java.task11.controller.service;
 
-import com.java.task11.controller.dao.DaoFactory;
-import com.java.task11.model.Project;
-
 import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.java.task11.controller.dao.factory.DAOException;
+import com.java.task11.controller.dao.factory.DAOFactory;
+import com.java.task11.model.Project;
+import com.java.task11.webapp.LoginServlet;
+import com.sun.org.apache.regexp.internal.recompile;
 
 /**
  * @author nlelyak
  * @version 1.00 2014-03-05
  */
 public class ProjectService implements IBaseService<Project> {
+	private static Logger log = Logger.getLogger(LoginServlet.class);
+	@Override
+	public Project getByID(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public Project getByID(Integer id) {
-        return DaoFactory.getInstance().getProjectDao().getByID(id);
-    }
+	@Override
+	public void save(Project element) {
+		try {
+			DAOFactory.getInstance().getProjectDAO().insert(element);
+		} catch (DAOException e) {
+			log.error(e);
+		}
+	}
 
-    @Override
-    public void save(Project element) {
-        DaoFactory.getInstance().getProjectDao().save(element);
-    }
+	@Override
+	public void update(Project element) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void update(Project element) {
-        DaoFactory.getInstance().getProjectDao().update(element);
-    }
+	@Override
+	public void delete(Project element) {
+		try {
+			DAOFactory.getInstance().getProjectDAO().delete(element);
+		} catch (DAOException e) {
+			log.error(e);
+		}
+		
+	}
 
-    @Override
-    public void delete(Project element) {
-        DaoFactory.getInstance().getProjectDao().delete(element);
-    }
+	@Override
+	public List<Project> getListOfObjects() {
+		try {
+			return DAOFactory.getInstance().getProjectDAO().selectAll();
+		} catch (DAOException e) {
+			log.error(e);
+			return null;
+		}
+	}
 
-    @Override
-    public List<Project> getListOfObjects() {
-        return DaoFactory.getInstance().getProjectDao().getListOfObjects();
-    }
+  
 }
