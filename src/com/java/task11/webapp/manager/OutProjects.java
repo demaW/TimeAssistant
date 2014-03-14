7a3f5a61d8ffab13bcf665c47bb746fd68fb2db0
demaW <demavov@gmail.com>
2014-03-14 14:05:03 +0200

    web.xml addings

---
+++ src/com/java/task11/webapp/manager/OutProjects.java
@@ -0,0 +1,52 @@
+package com.java.task11.webapp.manager;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.java.task11.controller.service.EmployeeService;
+import com.java.task11.controller.service.ProjectService;
+import com.java.task11.model.Employee;
+import com.java.task11.model.Project;
+
+/**
+ * Servlet implementation class OutProjects
+ */
+@WebServlet("/OutProjects")
+public class OutProjects extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OutProjects() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Project> projects  = new ProjectService().getListOfObjects();
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("projects", projects);
+		request.getRequestDispatcher("/pages/manager/projects.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
