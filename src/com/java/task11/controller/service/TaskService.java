b0811a8de9ad9385d2fbb48a997590c99714ac3e
Nazar Lelyak <nazar.lelyak@gmail.com>
2014-03-24 20:31:57 +0200

    add delete method to TaskService + add to text.properties file

--- src/com/java/task11/controller/service/TaskService.java
+++ src/com/java/task11/controller/service/TaskService.java
@@ -1,12 +1,15 @@
 package com.java.task11.controller.service;
 
-import java.util.List;
-
 import com.java.task11.controller.dao.factory.DAOException;
 import com.java.task11.controller.dao.factory.DAOFactory;
 import com.java.task11.model.Task;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServlet;
+import java.util.List;
 
 public class TaskService implements IBaseService<Task> {
+    private static Logger log = Logger.getLogger(TaskService.class);
 
 	@Override
 	public Task getByID(Integer id) throws DAOException {
@@ -40,4 +43,21 @@ public class TaskService implements IBaseService<Task> {
 	public List<Task> getByProjectId(Integer projectId) throws DAOException {
 		return DAOFactory.getInstance().getTaskDAO().getByProjectId(projectId);
 	}
+
+    public void delete(int taskId, HttpServlet servlet) {
+        try {
+            Task task = getByID(taskId);
+            delete(task, servlet);
+        } catch (DAOException e) {
+            log.error(e);
+        }
+    }
+
+    public void delete(Task task, HttpServlet servlet) {
+        try {
+            delete(task);
+        } catch (DAOException e) {
+            log.error(e);
+        }
+    }
 }
